95df0cb36aa7b3313819b14cccf576d7ee50b8bf
Dzonixy <dzonixy@gmail.com>
2023-03-03 16:18:26 +0100

    new club address for testing

--- governance/program/src/constants.rs
+++ governance/program/src/constants.rs
@@ -2,4 +2,5 @@
 /// The original spl-governance program address
 pub const OG_SPL_GOVERNANCE: &str = "GovER5Lthms3bLBqWub97yVrMmEogzX7xNjdXpPPCVZw";
 /// UNQ-Club program's address
-pub const UNQ_CLUB_PROGRAM: &str = "UNQc364EDzKQqfSW1Fnhfam2fvsFGVdVqg77kpBqRSe";
+// pub const UNQ_CLUB_PROGRAM: &str = "UNQc364EDzKQqfSW1Fnhfam2fvsFGVdVqg77kpBqRSe";
+pub const UNQ_CLUB_PROGRAM: &str = "C4m8NetipkF6RHB3bgErtVb376eDUdzeTYHnCgFhzvLU";
