e1ba01f3eef3bebde7ce11b890d2b8205305a16c
Dzonixy <dzonixy@gmail.com>
2023-03-09 17:01:08 +0100

    fixes address check

--- governance/program/src/constants.rs
+++ governance/program/src/constants.rs
@@ -4,3 +4,8 @@ pub const OG_SPL_GOVERNANCE: &str = "GovER5Lthms3bLBqWub97yVrMmEogzX7xNjdXpPPCVZ
 
 /// Club program's address
 pub const UNQ_CLUB_PROGRAM: &str = "6NZ2CekraPqUnkpvgQVQVZFVBjJgscqRvwn3wEmy3FPs";
+/// UNQ-Club program's address
+// pub const UNQ_CLUB_PROGRAM: &str = "UNQc364EDzKQqfSW1Fnhfam2fvsFGVdVqg77kpBqRSe";
+pub const UNQ_CLUB_PROGRAM_DEV: &str = "C4m8NetipkF6RHB3bgErtVb376eDUdzeTYHnCgFhzvLU";
+/// UNQ-Club program's address
+pub const UNQ_CLUB_PROGRAM_TEST: &str = "testPzmCCy4PgEyurCufhEEmxtHTGT9tu8VGgAD1qrR";
