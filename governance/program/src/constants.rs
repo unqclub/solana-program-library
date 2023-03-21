e82927e8eb213b4a7588ce0432678ffdb254b37d
Dzonixy <dzonixy@gmail.com>
2023-03-21 12:07:20 +0100

    setting syndicate address

--- governance/program/src/constants.rs
+++ governance/program/src/constants.rs
@@ -3,4 +3,4 @@
 pub const OG_SPL_GOVERNANCE: &str = "GovER5Lthms3bLBqWub97yVrMmEogzX7xNjdXpPPCVZw";
 
 /// Club program's address
-pub const UNQ_CLUB_PROGRAM: &str = "SMBcYrFfPYRrZnQgKdVxy9jj6yfng3KGNTzaEvTRJSg";
+pub const UNQ_CLUB_PROGRAM: &str = "6NZ2CekraPqUnkpvgQVQVZFVBjJgscqRvwn3wEmy3FPs";
