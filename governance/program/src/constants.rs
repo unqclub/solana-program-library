f24b61c9b8675a4c0aaf40995f94abfca18cbc17
Dzonixy <dzonixy@gmail.com>
2023-04-05 15:08:46 +0200

    fixes constant adresses

--- governance/program/src/constants.rs
+++ governance/program/src/constants.rs
@@ -1,15 +1,6 @@
 //! Constants
-/// The original spl-governance program address
-pub const OG_SPL_GOVERNANCE: &str = "GovER5Lthms3bLBqWub97yVrMmEogzX7xNjdXpPPCVZw";
-
 /// Club program's address
-pub const UNQ_CLUB_PROGRAM: &str = "6NZ2CekraPqUnkpvgQVQVZFVBjJgscqRvwn3wEmy3FPs";
-/// UNQ-Club program's address
-// pub const UNQ_CLUB_PROGRAM: &str = "UNQc364EDzKQqfSW1Fnhfam2fvsFGVdVqg77kpBqRSe";
-pub const UNQ_CLUB_PROGRAM_DEV: &str = "C4m8NetipkF6RHB3bgErtVb376eDUdzeTYHnCgFhzvLU";
-/// UNQ-Club program's address
-pub const UNQ_CLUB_PROGRAM_TEST: &str = "testPzmCCy4PgEyurCufhEEmxtHTGT9tu8VGgAD1qrR";
-/// UNQ-Club program's address
-pub const UNQ_CLUB_PROGRAM_MAIN: &str = "UNQc364EDzKQqfSW1Fnhfam2fvsFGVdVqg77kpBqRSe";
+pub const UNQ_CLUB_PROGRAM: &str = "SMBcYrFfPYRrZnQgKdVxy9jj6yfng3KGNTzaEvTRJSg";
+
 /// UNQ-Club program's authority
-pub const UNQ_CLUB_AUTHORITY: &str = "UNQdVcqxKicj3TXq8zYcFdFiUPTb65AjguJRSDjPHpk";
+pub const UNQ_CLUB_AUTHORITY: &str = "SMBdSeXkNzhw7WASd8qHMuUfKJSxbVmPrAmibJUAEiy";
