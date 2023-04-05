9a741bd2255ff0323817e7e430c0bca34e2062a6
Dzonixy <dzonixy@gmail.com>
2023-04-05 15:18:21 +0200

    sets constants up

--- governance/program/src/constants.rs
+++ governance/program/src/constants.rs
@@ -1,6 +1,6 @@
 //! Constants
 /// Club program's address
-pub const UNQ_CLUB_PROGRAM: &str = "SMBcYrFfPYRrZnQgKdVxy9jj6yfng3KGNTzaEvTRJSg";
+pub const UNQ_CLUB_PROGRAM: &str = "BGLcqDXMaWwAXfu8FbabngFdyEURKyk3Z72MengPifu1";
 
 /// UNQ-Club program's authority
-pub const UNQ_CLUB_AUTHORITY: &str = "SMBdSeXkNzhw7WASd8qHMuUfKJSxbVmPrAmibJUAEiy";
+pub const UNQ_CLUB_AUTHORITY: &str = "BGLdd1UHP8rdMhtrXxaf4ZJC7G5EspYTdsCxJnk3o3MZ";
