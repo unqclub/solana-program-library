0b3db9f38f6f98a2af0c6597332d8a1d580df6d4
unbound26z <draskovic26z@gmail.com>
2023-07-12 16:33:42 +0200

    account address change for devnet

--- governance/program/src/constants.rs
+++ governance/program/src/constants.rs
@@ -2,4 +2,4 @@
 /// The original spl-governance program address
 pub const OG_SPL_GOVERNANCE: &str = "GovER5Lthms3bLBqWub97yVrMmEogzX7xNjdXpPPCVZw";
 /// UNQ-Club program's address
-pub const UNQ_CLUB_PROGRAM: &str = "UNQc364EDzKQqfSW1Fnhfam2fvsFGVdVqg77kpBqRSe";
+pub const UNQ_CLUB_PROGRAM: &str = "refCApDnPuMw6MEZwjtMGsXTwTRafpuEDA3hgofNhZk";
